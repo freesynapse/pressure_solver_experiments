d22785b4c19ae98813a769982cf77008aeb7b3e6
freesynapse <mans.edstrom@gmail.com>
2023-11-14 22:50:18 +0100

    compute div

--- src/main.cpp
+++ src/main.cpp
@@ -28,11 +28,14 @@ public:
     bool m_wireframeMode = false;
     bool m_toggleCulling = false;
 
-    std::shared_ptr<Field1D> m_scalarField = nullptr;
-    std::shared_ptr<Field2D> m_vectorField = nullptr;
+    std::shared_ptr<Field2D> m_velocity = nullptr;
+    std::shared_ptr<Field1D> m_divergence = nullptr;
     std::shared_ptr<FieldRenderer> m_fieldRenderer = nullptr;
-    glm::ivec2 m_simShape = { 40, 40 };
+    glm::ivec2 m_shape = { 40, 40 };
     void onResize(Event *_e);
+
+    bool m_doRenderDiv = true;
+    bool m_doRenderVelocity = true;
 };
 
 //
@@ -47,9 +50,9 @@ Application* CreateSynapseApplication() { return new syn_app_instance(); }
 void layer::onResize(Event *_e)
 {
     ViewportResizeEvent *e = dynamic_cast<ViewportResizeEvent*>(_e);
-    m_fieldRenderer = std::make_shared<FieldRenderer>(m_simShape, e->getViewport());
-    m_fieldRenderer->setData1D(m_scalarField);
-    m_fieldRenderer->setData2D(m_vectorField);
+    m_fieldRenderer = std::make_shared<FieldRenderer>(m_shape, e->getViewport());
+    m_fieldRenderer->setData1D(m_divergence);
+    m_fieldRenderer->setData2D(m_velocity);
 }
 
 //----------------------------------------------------------------------------------------
@@ -71,57 +74,57 @@ void layer::onAttach()
     EventHandler::register_callback(EventType::VIEWPORT_RESIZE, SYN_EVENT_MEMBER_FNC(layer::onResize));
 
     //
-    m_scalarField = std::make_shared<Field1D>(m_simShape);
-    m_vectorField = std::make_shared<Field2D>(m_simShape);
+    Field1D in_data = Field1D(m_shape);
+    m_velocity = std::make_shared<Field2D>(m_shape);
 
     // 1D data -- something to point to...
-    float *f = m_scalarField->data();
-    for (int y = 0; y < m_simShape.y; y++)
+    float *f = in_data.data();
+    for (int y = 0; y < m_shape.y; y++)
     {
-        for (int x = 0; x < m_simShape.x; x++)
+        for (int x = 0; x < m_shape.x; x++)
         {
-            //f[y * m_simShape.x + x] = -(cosf(y * 2*M_PI / (float)m_simShape.y) + \
-            //                            cosf(x * 2*M_PI / (float)m_simShape.x));
-            float f_ = -(std::pow(y-(m_simShape.y*0.5f), 2) + std::pow(x-(m_simShape.x*0.5f), 2));
-            f[y * m_simShape.x + x] = f_;
+            //f[y * m_shape.x + x] = -(cosf(y * 2*M_PI / (float)m_shape.y) + \
+            //                            cosf(x * 2*M_PI / (float)m_shape.x));
+            float f_ = -(std::pow(y-(m_shape.y*0.5f), 2) + std::pow(x-(m_shape.x*0.5f), 2));
+            f[y * m_shape.x + x] = f_;
         }
     }
 
-    // 2D data
-    glm::vec2 *v = m_vectorField->data();
+    // Velocity from input data (2d monotonic decrease around origin)
+    glm::vec2 *v = m_velocity->data();
 
-    glm::vec2 dv2 = { 2.0f / (float)m_simShape.x, 2.0f / (float)m_simShape.y };
-    int n = m_simShape.x;
+    glm::vec2 dv2 = { 2.0f / (float)m_shape.x, 2.0f / (float)m_shape.y };
+    int n = m_shape.x;
 
     // TODO : replace with separate loops for borders and interior
-    for (int y = 0; y < m_simShape.y; y++)
+    for (int y = 0; y < m_shape.y; y++)
     {
-        for (int x = 0; x < m_simShape.x; x++)
+        for (int x = 0; x < m_shape.x; x++)
         {         
-            int idx = y * m_simShape.x + x;
+            int idx = y * m_shape.x + x;
             
             // borders
             //
             // top
-            if (y == 0 && x > 0 && x < m_simShape.x - 1)
+            if (y == 0 && x > 0 && x < m_shape.x - 1)
             {
                 v[idx].x = (f[idx+1] - f[idx-1]) / dv2.x;
                 v[idx].y = (f[idx+n] - f[idx]) / (0.5f * dv2.y);
             }
             // bottom
-            else if (y == m_simShape.y - 1 && x > 0 && x < m_simShape.x - 1)
+            else if (y == m_shape.y - 1 && x > 0 && x < m_shape.x - 1)
             {
                 v[idx].x = (f[idx+1] - f[idx-1]) / dv2.x;
                 v[idx].y = (f[idx] - f[idx-n]) / (0.5f * dv2.y);
             }
             // left
-            else if (x == 0 && y > 0 && y < m_simShape.y - 1)
+            else if (x == 0 && y > 0 && y < m_shape.y - 1)
             {
                 v[idx].x = (f[idx+1] - f[idx]) / (0.5f * dv2.x);
                 v[idx].y = (f[idx+n] - f[idx-n]) / dv2.y;
             }
             // right
-            else if (x == m_simShape.x - 1 && y > 0 && y < m_simShape.y - 1)
+            else if (x == m_shape.x - 1 && y > 0 && y < m_shape.y - 1)
             {
                 v[idx].x = (f[idx] - f[idx-1]) / (0.5f * dv2.x);
                 v[idx].y = (f[idx+n] - f[idx-n]) / dv2.y;
@@ -129,19 +132,40 @@ void layer::onAttach()
 
             // interior
             //
-            else if (x > 0 && x < m_simShape.x - 1 && y > 0 && y < m_simShape.y - 1)
+            else if (x > 0 && x < m_shape.x - 1 && y > 0 && y < m_shape.y - 1)
             {
                 v[idx].x = (f[idx+1] - f[idx-1]) / dv2.x;
                 v[idx].y = (f[idx+n] - f[idx-n]) / dv2.y;
             }
 
-            else
-            {
-                v[idx] = glm::vec2(0.0f);
-            }
-
         }
     
+    }
+
+    // set corners
+    v[0] = 0.5f * (v[1] + v[m_shape.x]);
+    v[(m_shape.y-1)*m_shape.x+0] = 0.5f * (v[(m_shape.y-1)*m_shape.x+1] + v[(m_shape.y-2)*m_shape.x+0]);
+    v[m_shape.x-1] = 0.5f * (v[m_shape.x-2] + v[m_shape.x + m_shape.x-1]);
+    v[(m_shape.y-1)*m_shape.x+m_shape.x-1] = 0.5f * (v[(m_shape.y-2)*m_shape.x+m_shape.x-1] + v[(m_shape.y-1)*m_shape.x+m_shape.x-2]);
+
+    // Compute the divergence from the velocity field
+    //
+    m_divergence = std::make_shared<Field1D>(m_shape);
+    f = m_divergence->data();
+    float h = 1.0f / (float)m_shape.y;
+    float div_mult = 1.0f / (2 * h);
+    for (int y = 0; y < m_shape.y; y++)
+    {
+        for (int x = 0; x < m_shape.x; x++)
+        {
+            int idx = y * m_shape.x + x;
+
+            if (x == 0 || y == 0 || x == m_shape.x - 1 || y == m_shape.y - 1)
+                f[idx] = 0.0f;
+            else
+                f[idx] = div_mult * (v[idx+1].x - v[idx-1].x + v[idx+m_shape.x].y - v[idx-m_shape.x].y);
+        }
+
     }
 
     // general settings
@@ -168,7 +192,9 @@ void layer::onUpdate(float _dt)
 
     if (m_fieldRenderer)
     {
-        // m_fieldRenderer->renderField1D();
+        if (m_doRenderDiv)
+            m_fieldRenderer->renderField1D();
+        if (m_doRenderVelocity)
         m_fieldRenderer->renderField2D();
     }
 
@@ -207,6 +233,15 @@ void layer::onKeyDownEvent(Event *_e)
             case SYN_KEY_ESCAPE:    EventHandler::push_event(new WindowCloseEvent()); break;
             case SYN_KEY_F4:        m_wireframeMode = !m_wireframeMode; break;
             case SYN_KEY_F5:        m_toggleCulling = !m_toggleCulling; Renderer::setCulling(m_toggleCulling); break;
+
+            case SYN_KEY_1:
+                m_doRenderVelocity = !m_doRenderVelocity;
+                break;
+            
+            case SYN_KEY_2:
+                m_doRenderDiv = !m_doRenderDiv;
+                break;
+                
             default: break;
         }
     }
